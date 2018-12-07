dbd52defdca306baf140ee39fb59d73e9f85b0ac
semvs <semvs@ayvansaray.edu.tr>
2018-12-07 10:46:58 +0300

    beni yakala oyunu

---
+++ MouseButonOyunu/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace MouseButonOyunu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bests = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(136, 172);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // bests
+            // 
+            this.bests.AutoSize = true;
+            this.bests.Location = new System.Drawing.Point(60, 9);
+            this.bests.Name = "bests";
+            this.bests.Size = new System.Drawing.Size(13, 13);
+            this.bests.TabIndex = 2;
+            this.bests.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(-2, 35);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "score";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(-2, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "best score";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(375, 411);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.bests);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label bests;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ MouseButonOyunu/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//best score tut.
+namespace MouseButonOyunu
+{
+    public partial class Form1 : Form
+    {
+        int best=int.MaxValue;
+        int counter ;
+        int x, y;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        public void degis(int a,int b)
+        {
+            button1.Location = new Point(x, y);
+        }
+
+//butona tıklandıgında kazandınız mesajı yazsın.
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Best(best);
+            counter =0;
+            MessageBox.Show("tebrikler kazandınız.");
+            label1.Text = "0";
+
+            
+        }
+
+//mouse butona değdiği zaman buton yeniden konumlandırılsın.
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            x = rnd.Next(ClientSize.Width-100);
+            y = rnd.Next(ClientSize.Height-100);
+            degis(x, y);
+        }
+
+//forma tıklandıgında sayac artsın.
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            counter++;
+            string ycounter = Convert.ToString(counter);
+            label1.Text = ycounter;
+           // best = counter;
+            
+
+
+        }
+        public void Best(int x)
+        {
+            if (counter < best)
+            {
+                best = counter;
+                bests.Text = Convert.ToString(best);
+            }
+        }
+
+    }
+}
